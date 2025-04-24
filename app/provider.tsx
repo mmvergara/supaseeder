9c37cf76c230ea41badb9b2089270158a6092eac
mmvergara <mark.jesusmanabat@gmail.com>
2025-04-24 10:28:57 +0800

    add PostHogPageView component for manual pageview tracking; integrate into PostHogProvider; remove vercel.json

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,21 +3,27 @@
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useEffect } from "react";
+import PostHogPageView from "./PostHogPageView";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const postHogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-    if (!postHogKey) {
+    const postHogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+    if (!postHogKey || !postHogHost) {
       console.error("PostHog key or host is not defined, skipping posthog.");
       return;
     }
 
     posthog.init(postHogKey, {
-      api_host: "https://supaseeder.vercel.app/ingest",
-      ui_host: "https://us.posthog.com",
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      api_host: postHogHost,
+      capture_pageview: false,
     });
   }, []);
 
-  return <PHProvider client={posthog}>{children}</PHProvider>;
+  return (
+    <PHProvider client={posthog}>
+      <PostHogPageView />
+      {children}
+    </PHProvider>
+  );
 }
